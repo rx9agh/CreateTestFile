3dd3ae174d028be399f1e12cf2607df7f1a2b0c3
Dmitry <rx9agh@gmail.com>
2025-02-10 17:42:30 +0400

    created

---
+++ CreateTestFile/Program.cs
@@ -0,0 +1,19 @@
+﻿
+string fileName = "test.txt";
+int countOfLines = 1000_000;
+
+string[] strings = [ "Apple test", "Carrot", "Banana", "Orange", "Grapes", 
+"Pineapple", "Watermelon", "Strawberry", "Blueberry", "Potato" ];
+
+if(args.Length > 0 && args[0] != string.Empty && int.TryParse(args[0], out int val))
+    countOfLines = val;
+
+using StreamWriter writer = new(fileName, false);
+var rand = new Random();
+List<int> listNumbers = [.. Enumerable.Range(1, countOfLines).OrderBy(i => rand.Next())];
+
+foreach (int number in listNumbers)
+{
+    int rn = rand.Next(0, strings.Length - 1);
+    await writer.WriteLineAsync($"{number}. {strings[rn]}");
+}
