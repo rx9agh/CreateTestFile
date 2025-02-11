b29e750579f432fe012053621a54e3dec1465736
rx9agh <rx9agh@gmail.com>
2025-02-11 15:19:01 +0400

    Update Program.cs

--- CreateTestFile/Program.cs
+++ CreateTestFile/Program.cs
@@ -2,18 +2,23 @@
 string fileName = "test.txt";
 int countOfLines = 1000_000;
 
-string[] strings = [ "Apple test", "Carrot", "Banana", "Orange", "Grapes", 
+string[] stringPartOfLines = [ "Apple test", "Carrot", "Banana", "Orange", "Grapes", 
 "Pineapple", "Watermelon", "Strawberry", "Blueberry", "Potato" ];
 
-if(args.Length > 0 && args[0] != string.Empty && int.TryParse(args[0], out int val))
-    countOfLines = val;
+if(args.Length > 0 && args[0] != string.Empty && int.TryParse(args[0], out int lines))
+    countOfLines = lines;
 
 using StreamWriter writer = new(fileName, false);
 var rand = new Random();
-List<int> listNumbers = [.. Enumerable.Range(1, countOfLines).OrderBy(i => rand.Next())];
+List<int> randomNumbers = [.. Enumerable.Range(1, countOfLines).OrderBy(i => rand.Next())];
 
-foreach (int number in listNumbers)
+int count = 1;
+foreach (int numberPartOfLine in randomNumbers)
 {
-    int rn = rand.Next(0, strings.Length - 1);
-    await writer.WriteLineAsync($"{number}. {strings[rn]}");
+    int rn = rand.Next(0, stringPartOfLines.Length - 1);
+    await writer.WriteLineAsync($"{numberPartOfLine}. {stringPartOfLines[rn]}");
+
+    if(count%10000 == 0)
+        Console.Write("|");
+    count++;
 }
